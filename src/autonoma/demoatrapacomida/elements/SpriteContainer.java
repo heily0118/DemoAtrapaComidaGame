cadd2c283efdeb24438725d0ec20da45052fd8ea
Heily Rios <heilyayala005@gmail.com>
2025-05-16 13:20:49 -0500

    Se crearon nuevas clases

--- src/autonoma/demoatrapacomida/elements/SpriteContainer.java
+++ src/autonoma/demoatrapacomida/elements/SpriteContainer.java
@@ -5,9 +5,96 @@
 package autonoma.demoatrapacomida.elements;
 
 /**
- *
- * @author USUARIO
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@gmail.com>
+ * @since 20250430
+ * @version 1.0.0
  */
-public class SpriteContainer {
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+public abstract class SpriteContainer implements GraphicContainer {
+    /**
+     * Coordenada horizontal del contenedor.
+     */
+    protected int x;
+
+    /**
+     * Coordenada vertical del contenedor.
+     */
+    protected int y;
+
+    /**
+     * Alto del contenedor.
+     */
+    protected int height;
+
+    /**
+     * Ancho del contenedor.
+     */
+    protected int width;
+    /**
+     * Lista de sprites contenidos en este contenedor.
+     */
+    protected ArrayList<Sprite> sprites;
+
+    /**
+     * @param x      coordenada horizontal del contenedor
+     * @param y      coordenada vertical del contenedor
+     * @param height alto del contenedor
+     * @param width  ancho del contenedor
+     */
+    public SpriteContainer(int x, int y, int height, int width) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        sprites = new ArrayList<>();
+    }
+
+    /** 
+     * @param sprite sprite a agregar
+     * @return true si fue agregado correctamente
+     */
+    public boolean add(Sprite sprite) {
+        return sprites.add(sprite);
+    }
+    /**
+     * @param index índice del sprite a eliminar
+     */
+    public void remove(int index) {
+        sprites.remove(index);
+    }
+
+    /**
+     * 
+     * @param sprite sprite a eliminar
+     */
+    public void remove(Sprite sprite) {
+        sprites.remove(sprite);
+    }
+
+    /**
+     * @return número de sprites
+     */
+    public int size() {
+        return sprites.size();
+    }
+
+    @Override
+    public void refresh(Graphics g) {
+       g.setColor(Color.WHITE);
+       g.fillRect(0, 0,  width, height);
+
+     
+
+    }
+
+    @Override
+    public Rectangle getBoundaries() {
+        return new Rectangle(x, y, width, height);
+    }
     
 }
