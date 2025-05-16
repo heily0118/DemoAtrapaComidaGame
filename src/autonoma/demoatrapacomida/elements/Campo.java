90819ee3e9b6a5561983083db68feaabe52eb95c
mateo <mateoq152006@gmail.com>
2025-05-16 14:14:29 -0500

    se crea la clase de campo

---
+++ src/autonoma/demoatrapacomida/elements/Campo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.demoatrapacomida.elements;
+
+import java.util.ArrayList;
+
+    
+  /**
+ *
+  * @author Mateo Quintero <mateo.quinterom@autonoma.edu.co>
+ * @version 1.0.0
+ * @since 20250501
+ * @see autonoma.emoatrapacomida..elements
+ */
+public class Campo {
+
+    ////////////////////////////////
+    /// Atributos
+    ///
+    public ArrayList<Comida> comidas;
+    public ArrayList<Veneno> venenos;
+    public Jugador jugador;
+
+    public Campo(Jugador jugador) {
+        this.jugador = jugador;
+        this. comidas  = new ArrayList<>();
+        this.venenos = new ArrayList<>();
+        
+    }
+
+    
+}
