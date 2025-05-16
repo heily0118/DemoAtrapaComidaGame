d2550a2b6d55be9183b5fd330e5bc3eec4279d69
mateo <mateoq152006@gmail.com>
2025-05-16 13:48:31 -0500

    crea la calse de puntaje

---
+++ src/autonoma/demoatrapacomida/elements/Puntaje.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.demoatrapacomida.elements;
+
+/**
+ *
+  * @author Mateo Quintero <mateo.quinterom@autonoma.edu.co>
+ * @version 1.0.0
+ * @since 20250501
+ * @see autonoma.emoatrapacomida..elements
+ */
+public class Puntaje {
+
+    ////////////////////////////////
+    /// Atributos
+    ///
+    private int puntajeActual;
+
+
+    /////////////////////////////////
+    /// Constructor
+    ////
+    
+    public Puntaje() {
+        this.puntajeActual = 0;
+    }
+
+    
+    //////////////////////////////////
+    /// Metodos de acceso (set)
+    ///
+    
+    public void setPuntajeActual(int puntajeActual) {
+        this.puntajeActual = puntajeActual;
+    }
+
+    //////////////////////////////////
+    /// Metodos de acceso (get)
+    ///
+    public int getPuntajeActual() {
+        return puntajeActual;
+    }
+
+
+    //////////////////////////////////
+    /// Metodos
+    ///
+
+    public void aumentarPuntaje(int valor){
+        
+        this.puntajeActual += valor;
+    }
+    public void dismuirPuntaje(int valor ){
+    
+        this.puntajeActual -= valor;
+    
+    }
+
+    
+    
+}
