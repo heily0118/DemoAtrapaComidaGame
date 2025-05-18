c80de7a5a402574392749cbc1818f3d2fced1954
mateo <mateoq152006@gmail.com>
2025-05-17 20:44:03 -0500

    se ponde sonido en la ventana principal

--- src/autonoma/demoatrapacomida/gui/VentanaPrincipal.java
+++ src/autonoma/demoatrapacomida/gui/VentanaPrincipal.java
@@ -9,6 +9,12 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -23,8 +29,12 @@ public class VentanaPrincipal extends javax.swing.JFrame {
   
     private VideoJuego juego;
     
+    private Clip clip;
+    
     public VentanaPrincipal(VideoJuego juego) {
         this.juego = juego;
+        
+        reproducirSonido();
         setTitle("Atrapa Comida");
         setSize(800, 800);
         setLocationRelativeTo(null);
@@ -103,6 +113,27 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
 
+    public void reproducirSonido() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                    getClass().getResource("/autonoma/demoatrapacomida/sounds/Menu.wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public void detenerSonido() {
+        if (clip != null) {
+            clip.stop();
+            clip.close();
+        }
+    }
+    
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
